6d3adf9767663d04003a6fbd4bbb0050bcb34027
TJonCanon <taylor.joyner93@me.com>
2023-09-06 22:46:08 -0500

    pycodestyle

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -2,6 +2,9 @@
 """ function given to be annotated """
 from typing import List, Tuple, Iterable, Sequence
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+
+
     """ element_length function declaration"""
     return [(i, len(i)) for i in lst]
