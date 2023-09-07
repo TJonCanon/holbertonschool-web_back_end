d31e0a0705c0aadc90413d895cd831830ab03498
TJonCanon <taylor.joyner93@me.com>
2023-09-06 22:14:18 -0500

    task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" concatonates a string """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ concats multiple strings and returns a new one """
+    return str1 + str2
