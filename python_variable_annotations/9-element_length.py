7b88ffea094899821c27fd88f50c358676653499
TJonCanon <taylor.joyner93@me.com>
2023-09-06 22:38:33 -0500

    task 9

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" function given to be annotated """
+from typing import List, Tuple, Iterable, Sequence
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """ element_length function declaration"""
+    return [(i, len(i)) for i in lst]
