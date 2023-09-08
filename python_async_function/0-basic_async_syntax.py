51fa6eaccfe36dccb74d1453355c3083a973b60e
TJonCanon <taylor.joyner93@me.com>
2023-09-07 19:51:25 -0500

    task 0

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""  asynchronous coroutine that takes
+    an integer, waits, then returns it """
+
+
+import asyncio
+from random import uniform
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """ returns random number"""
+    delay = uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
