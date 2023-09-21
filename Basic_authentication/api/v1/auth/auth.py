06d535fcc46082d264aecbfedf09d12e74c88eb3
TJonCanon <taylor.joyner93@me.com>
2023-09-20 22:46:55 -0500

    task 3

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -9,13 +9,19 @@ import os
 
 class Auth():
     """ Auth class for authentication """
+from flask import request
+from typing import List, TypeVar
+import os
+
+
+class Auth():
+    """ Auth class for authentication """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ returns True if authenticated, else False"""
-        if not path or not path or excluded_paths:
+        if not path or not excluded_paths:
             return True
-        return False if os.path.join(path,
-        '') in excluded_paths else True
+        return False if os.path.join(path, '') in excluded_paths else True
 
     def authorization_header(self, request=None) -> str:
         """ returns Flask request """
