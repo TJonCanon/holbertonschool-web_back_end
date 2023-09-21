f2a1a8fd936f8cc1bc643d5640b6d40d9801de32
TJonCanon <taylor.joyner93@me.com>
2023-09-20 21:18:03 -0500

    pycodestyle

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -8,6 +8,7 @@ def hash_password(password: str) -> bytes:
     """ returns a salted, hashed password as a byte string """
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
 
+
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """ checks a hashed_password against the real deal """
     return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
