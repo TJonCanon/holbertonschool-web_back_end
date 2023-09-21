ad0c05855700df4ea053def78491e4b059132734
TJonCanon <taylor.joyner93@me.com>
2023-09-20 21:17:22 -0500

    task 6

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -7,3 +7,7 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """ returns a salted, hashed password as a byte string """
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ checks a hashed_password against the real deal """
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
