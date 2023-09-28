7cfbd7f6a4c325a24ac687f292a18b204475a20d
TJonCanon <taylor.joyner93@me.com>
2023-09-27 23:19:42 -0500

    task 18

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -74,3 +74,15 @@ class Auth:
         reset_token = _generate_uuid()
         self._db.update_user(user, reset_token=reset_token)
         return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """ update a users hashed_password """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            self._db.update_user(
+                user.id,
+                hashed_password=_hash_password(password),
+                reset_token=None
+            )
+        except NoResultFound:
+            raise ValueError
