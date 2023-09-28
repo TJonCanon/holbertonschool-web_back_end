96e34332bed1c1ea4d60dd1bcc8eab8b16c48d02
TJonCanon <taylor.joyner93@me.com>
2023-09-27 23:14:03 -0500

    task 14

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ another flask app! """
 
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 
@@ -38,6 +38,27 @@ def login():
     else:
         abort(401)
 
+@app.route("/sessions", methods=["DELETE"])
+def logout():
+    """ route for logouts """
+    session_id = request.cookies.get("session_id")
+    if session_id:
+        user = AUTH.get_user_from_session_id(session_id=session_id)
+        if user:
+            AUTH.destroy_session(user.id)
+            return redirect("/")
+    abort(403)
+
+@app.route('/profile', methods=['GET'])
+def profile():
+    """ route for profiles """
+    session_id = request.cookies.get('session_id')
+    if session_id:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            return jsonify({"email": user.email}), 200
+    else:
+        abort(403)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
