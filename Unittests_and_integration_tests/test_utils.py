7dbbf91cc5d580ffed72bfe1a79bec2d83dab78b
TJonCanon <taylor.joyner93@me.com>
2023-10-04 21:20:56 -0500

    fixed syntax

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,7 @@
 
 
 import unittest
-import unittest.mock import Mock, patch
+from unittest.mock import Mock, patch
 from parameterized import parameterized
 from utils import access_nested_map, get_json
 
