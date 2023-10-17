f7f2c39032335f50b32a1b595992bbeff2e5df7c
TJonCanon <taylor.joyner93@me.com>
2023-10-17 15:25:59 -0500

    task 1

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 
 import redis
-from typing import Union
+from typing import Union, callable, Optional
 import uuid
 
 
@@ -19,3 +19,23 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[
+        str,
+        bytes,
+        int,
+        float
+    ]:
+        """ get from redis """
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """ get string from redis """
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """ get int from redis """
+        return self.get(key, int)
