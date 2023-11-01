dfbdb20275cc5d114833babbc7e55de8c31f591a
TJonCanon <taylor.joyner93@me.com>
2023-11-01 15:32:05 -0500

    task 1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+// displays welcome message for new users
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin
+  .on('readable', () => {
+    const name = process.stdin.read();
+    if (name) {
+      process.stdout.write(`Your name is: ${name}`);
+  }
+})
+  .on('end', () => {
+    process.stdout.write('This important software is now closing\n');
+});
