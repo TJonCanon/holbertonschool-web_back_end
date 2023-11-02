71ba45c0c76923dc577d0f42695a47d3831311ff
TJonCanon <taylor.joyner93@me.com>
2023-11-01 20:09:42 -0500

    task 6

---
+++ unittests_in_js/6-payment_token.js
@@ -0,0 +1,11 @@
+// returns promise from api
+const getPaymentTokenFromAPI = (success) => {
+    if (success) {
+      return new Promise((resolve, reject) => {
+        resolve({ data: 'Successful response from the API' });
+      });
+    }
+  };
+  
+  module.exports = getPaymentTokenFromAPI;
+  
