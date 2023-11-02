a4500b27779a3e11c7fa0f893244e152d7703eac
TJonCanon <taylor.joyner93@me.com>
2023-11-01 20:16:16 -0500

    task 9

---
+++ unittests_in_js/9-api/api.js
@@ -0,0 +1,21 @@
+// create an express app
+const express = require('express');
+const app = express();
+
+app
+  .get('/', (req, res) => {
+    res.send('Welcome to the payment system');
+  })
+  .get('/cart/:id', (req, res) => {
+    const id = req.params.id;
+    if (isNaN(id)) {
+      res.status(404).send('id must be a number');
+    } else {
+      res.send(`Payment methods for cart ${id}`);
+    }
+  })
+  .listen(7865, () => {
+    console.log('API available on localhost port 7865');
+  });
+
+module.exports = app;
